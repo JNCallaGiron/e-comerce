59c8410f029bc39d034a368e6308513ad2e63727
JNCallaGiron <jairocalla3@gmail.com>
2025-07-01 18:10:43 -0300

    mostrar la vista del producto al usuario

--- src/main/java/com/jairo/spring_ecomerce/controller/HomeController.java
+++ src/main/java/com/jairo/spring_ecomerce/controller/HomeController.java
@@ -2,14 +2,17 @@ package com.jairo.spring_ecomerce.controller;
 
 import com.jairo.spring_ecomerce.service.IProductoService;
 import jakarta.persistence.Id;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class HomeController {
 
     @Autowired
@@ -18,8 +21,13 @@ public class HomeController {
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("productos", productoService.listProductos());
-
-
         return "usuario/home";
+    }
+
+    //de ver producto a una nueva vista
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Long id){
+        log.info("Id  producto enviado como parametro {}", id);
+        return "usuario/productohome";
     }
 }
